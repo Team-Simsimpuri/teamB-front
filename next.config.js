5ade97295c72891ebab51dbe1302a56a12dacdb6
robin <nowrobin3@gmail.com>
2023-12-24 14:43:34 +0900

    a

--- next.config.js
+++ next.config.js
@@ -1,4 +1,37 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+// const withPlugins = require("next-compose-plugins");
+// const withPWA = require("next-pwa");
+
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// module.exports = nextConfig;
+
+// const prod = process.env.NODE_ENV === "production";
+
+// const runtimeCaching = require("next-pwa/cache");
+// const withPWA = require("next-pwa")({
+//   dest: "public",
+//   register: true,
+//   skipWaiting: true,
+//   runtimeCaching,
+//   disable: prod ? false : true,
+// });
+
+const prod = process.env.NODE_ENV === "production";
+
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  disable: prod ? false : true,
+});
+
+const nextConfig = withPWA({
+  // next config
+});
+module.exports = nextConfig;
